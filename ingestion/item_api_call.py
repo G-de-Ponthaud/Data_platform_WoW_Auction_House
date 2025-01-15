0355340d1fcfb086e778a21016d7b87a64679f96
G-de-Ponthaud <gaute@DESKTOP-1L5S66R.>
2025-01-15 14:30:16 +0100

    add Python code to ingest all items name and caracteristics using a csv file

---
+++ ingestion/item_api_call.py
@@ -0,0 +1,39 @@
+import requests
+import os
+import pandas
+
+min_id_in_page = 1
+access_token = os.environ["ACCESS_TOKEN"]
+next_page = True
+idList = []
+nameList = []
+itemSubClassList = []
+itemClassList = []
+qualityList = []
+
+while next_page == True:
+
+    url = f"https://eu.api.blizzard.com/data/wow/search/item?namespace=static-eu&orderby=id&_pageSize=1000&_page=1&id=[{str(min_id_in_page)},]"
+
+    result = requests.get(url,
+        headers={'Content-Type':'application/json',
+                'Authorization': 'Bearer {}'.format(access_token)})
+    json_result = result.json()
+    json_result=json_result["results"]
+    
+    for item in json_result :
+        idList.append(item.get("data", {}).get("media", {}).get("id", None))
+        nameList.append(item.get("data", {}).get("name", {}).get("en_US", None))
+        itemSubClassList.append(item.get("data", {}).get("item_subclass", {}).get("name", {}).get("en_US", None))
+        itemClassList.append(item.get("data", {}).get("item_class", {}).get("name", {}).get("en_US", None))
+        qualityList.append(item.get("data", {}).get("quality", {}).get("name", {}).get("en_US", None))
+
+    if min_id_in_page == idList[len(idList)-1]+1 :
+        next_page = False
+    else :
+        min_id_in_page = idList[len(idList)-1]+1
+
+df = pandas.DataFrame({'id': idList, 'name': nameList, "subClass": itemSubClassList, 'class': itemClassList, 'quality': qualityList})
+df.set_index('id',inplace=True)
+print(df.first)
+df.to_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../dbt_WoW_Auction_House/seeds/item_list.csv"))
